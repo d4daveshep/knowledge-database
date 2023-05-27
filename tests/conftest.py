5f121bebafd5e2de78cbf7f5bcad77ed18d86cd9
d4daveshep <david@daveshep.net.nz>
2023-05-27 13:06:29 +1200

    set up connections data in database

--- tests/conftest.py
+++ tests/conftest.py
@@ -47,9 +47,38 @@ def db_cursor(db_connection):
 @pytest.fixture()
 def db_populate(db_cursor):
     names = [(1, "Andrew"), (2, "Brian"), (3, "Cindy")]
-    roles = [(11, "Chief Engineer"), (12, "Practice Lead")]
+    titles = [(11, "Chief Engineer"), (12, "Practice Lead"), (13, "Senior Engineer")]
     skills = [(21, "Java"), (22, "Angular"), (23, "React"), (24, "Android"), (25, "SpringBoot")]
     customers = [(31, "Warehouse Group"), (32, "Countdown"), (33, "Westpac"), (34, "Global Dairy")]
+
+    appointment_name = "has title"
+    appointments = [
+        (1, appointment_name, 1, 11),  # Andrew has title Chief Engineer
+        (2, appointment_name, 2, 12),  # Brian has title Practice Lead
+        (3, appointment_name, 3, 13),  # Cindy has title Senior Engineer
+    ]
+
+    experience_name = "has experience in"
+    experiences = [
+        (11, experience_name, 1, 21),  # Andrew has experience in Java
+        (12, experience_name, 1, 25),  # Andrew has experience in SpringBoot
+        (13, experience_name, 1, 23),  # Andrew has experience in Angular
+        (14, experience_name, 2, 21),  # Brian has experience in Java
+        (15, experience_name, 2, 24),  # Brian has experience in Android
+        (16, experience_name, 3, 22),  # Cindy has experience in Angular
+        (17, experience_name, 3, 23),  # Cindy has experience in React
+    ]
+
+    assignment_name = "worked at"
+    assignments = [
+        (21, assignment_name, 1, 31),  # Andrew worked at Warehouse Group
+        (22, assignment_name, 1, 34),  # Andrew worked at Global Dairy
+        (23, assignment_name, 2, 32),  # Brian worked at Countdown
+        (24, assignment_name, 2, 33),  # Brian worked at Westpac
+        (25, assignment_name, 3, 31),  # Cindy worked at Warehouse Group
+        (26, assignment_name, 3, 33),  # Cindy worked at Westpac
+        (27, assignment_name, 3, 34),  # Cindy worked at Global Dairy
+    ]
 
     """
     Here's how to generate a list of tuples of any length
@@ -57,7 +86,10 @@ def db_populate(db_cursor):
     nodes = [tuple([next(id_generator), node_name]) for node_name in names + roles + skills + customers]
     """
 
-    db_cursor.executemany("INSERT INTO nodes VALUES ( ?, ? )", names + roles + skills + customers)
+    db_cursor.executemany("INSERT INTO nodes VALUES ( ?, ? )", names + titles + skills + customers)
+    db_cursor.connection.commit()
+
+    db_cursor.executemany("INSERT INTO connections VALUES ( ?, ?, ?, ?)", appointments + experiences + assignments)
     db_cursor.connection.commit()
 
     return db_cursor
--- tests/test_db_setup.py
+++ tests/test_db_setup.py
@@ -1,4 +1,4 @@
-def test_db_setup(db_cursor):
+def test_db_tables_setup(db_cursor):
     result = db_cursor.execute("SELECT name FROM sqlite_master")
     assert result.fetchall() == [("nodes",), ("connections",)]
 
@@ -7,7 +7,11 @@ def test_db_setup(db_cursor):
 
 def test_db_populated(db_populate):
     result = db_populate.execute("SELECT name FROM nodes")
-    names = result.fetchall()
-    assert len(names) == 14
+    node_names = result.fetchall()
+    assert len(node_names) == 15
+
+    result = db_populate.execute("SELECT name, subject_id, target_id FROM connections")
+    connections = result.fetchall()
+    assert len(connections) == 17
 
     # assert names[0] == ("Andrew",)
