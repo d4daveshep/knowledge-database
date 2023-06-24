07cc7fc54aca3ac9f3686d16fffa8722ebb36560
d4daveshep <david@daveshep.net.nz>
2023-06-24 15:38:19 +1200

    write search requirements

--- tests/functional/test_search_page.py
+++ tests/functional/test_search_page.py
@@ -11,4 +11,12 @@ def test_search_page(my_base_url: str, page: Page):
     heading = page.get_by_role("heading", name="Search")
     expect(heading).to_have_text("Search")
 
+    # there is a text field labeled Search
+
+    # and a submit button
+
+    # when I enter a node name and hit submit I go to the Conections to a Node page
+
+    # and at least one node I searched for is there
+
     assert False
