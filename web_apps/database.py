afe5fb1b38272c45b13722d32d3d603973e52857
d4daveshep <david@daveshep.net.nz>
2023-06-25 17:49:55 +1200

    tested database stats page with test.db

--- web_apps/database.py
+++ web_apps/database.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./web_apps.db"
+db_file = os.environ.get("KNOWLEDGE_DB_FILE", default="live.db")
+SQLALCHEMY_DATABASE_URL = "sqlite:///" + db_file
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./web_apps.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine: Engine = create_engine(
