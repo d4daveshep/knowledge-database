f313ecb9e0b1844e8284e1b505b6ed71e11875b4
d4daveshep <david@daveshep.net.nz>
2023-06-30 11:56:56 +1200

    next step is to add code to load the test data

--- tests/functional/test_test_database.py
+++ tests/functional/test_test_database.py
@@ -17,4 +17,22 @@ def test_purge_database_fixture(purge_database, my_base_url: str, page: Page):
 
 
 def test_load_test_data(purge_database, my_base_url: str, page: Page):
-    pass
+    # browse to the (hidden) load test data page
+    page.goto(my_base_url+"/load-test-data")
+    expect(page).to_have_title("Load Test Data")
+
+    # click the button
+    load_button = page.get_by_role("button", name="Load Test Data")
+    load_button.click()
+
+    # should be at stats page
+    expect(page).to_have_title("Database Stats")
+
+    # confirm test data stats
+    node_count = page.get_by_text("Nodes")
+    expect(node_count).to_have_text("Nodes: 15")
+    connection_count = page.get_by_text("Connections")
+    expect(connection_count).to_have_text("Connections: 15")
+
+
+
