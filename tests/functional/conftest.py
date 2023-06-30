f265b0a910a3fd67948be5827feb57dd46afac1e
d4daveshep <david@daveshep.net.nz>
2023-06-30 15:49:33 +1200

    moved loading test data to a functional test fixture

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -15,3 +15,13 @@ def purge_database(my_base_url:str, page: Page):
     # click the confirm button
     purge_button = page.get_by_role("button", name="Purge Database")
     purge_button.click()
+
+@pytest.fixture
+def loaded_test_data(purge_database, my_base_url, page:Page):
+    # browse to the (hidden) load test data page
+    page.goto(my_base_url+"/load-test-data")
+    expect(page).to_have_title("Load Test Data")
+
+    # click the button
+    load_button = page.get_by_role("button", name="Load Test Data")
+    load_button.click()
--- tests/functional/test_test_database.py
+++ tests/functional/test_test_database.py
@@ -16,16 +16,9 @@ def test_purge_database_fixture(purge_database, my_base_url: str, page: Page):
     expect(connection_count).to_have_text("Connections: 0")
 
 
-def test_load_test_data(purge_database, my_base_url: str, page: Page):
-    # browse to the (hidden) load test data page
-    page.goto(my_base_url+"/load-test-data")
-    expect(page).to_have_title("Load Test Data")
+def test_load_test_data(loaded_test_data, my_base_url: str, page: Page):
 
-    # click the button
-    load_button = page.get_by_role("button", name="Load Test Data")
-    load_button.click()
-
-    # should be at stats page
+    page.goto(my_base_url + "/database-stats")
     expect(page).to_have_title("Database Stats")
 
     # confirm test data stats
