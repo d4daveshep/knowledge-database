aecf940630970119877cf12ab6b36279b9079779
d4daveshep <david@daveshep.net.nz>
2023-07-01 11:06:10 +1200

    test fails, need to wire the page up to the backend

--- tests/functional/test_search_page.py
+++ tests/functional/test_search_page.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import Page, expect
 
 
-def test_search_page(my_base_url: str, page: Page):
+def test_search_page(loaded_test_data, my_base_url: str, page: Page):
     page.goto(my_base_url + "/search")
 
     # title is Search
@@ -12,11 +12,19 @@ def test_search_page(my_base_url: str, page: Page):
     expect(heading).to_have_text("Search")
 
     # there is a text field labeled Search
+    search_field = page.get_by_label("Search")
+    expect(search_field).to_be_empty()
+    expect(search_field).to_be_editable()
 
     # and a submit button
+    submit_button = page.get_by_role("button", name="Search")
 
-    # when I enter a node name and hit submit I go to the Conections to a Node page
+    # when I enter a node name and hit submit I go to the Connections to a Node page
+    search_field.fill("Andrew")
+    submit_button.click()
+
+    expect(page).to_have_title("Connections to a Node")
 
     # and at least one node I searched for is there
 
-    assert False
+    # assert False
