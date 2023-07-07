30e99e30098d2388619bdfdabead8d6e6dceaaa4
d4daveshep <david@daveshep.net.nz>
2023-07-08 09:49:03 +1200

    not working yet - can't get connection results page to reappear after delete

--- tests/functional/test_delete_connection.py
+++ tests/functional/test_delete_connection.py
@@ -4,6 +4,7 @@ from playwright.sync_api import Page, expect
 def test_delete_single_connection(loaded_test_data, my_base_url:str, page: Page):
     # search for some connections
     page.goto(my_base_url+"/connections/?name_like=e")
+    # TODO get the number of connections so we can reduce by one
 
     # click the delete button for the 2nd row (Brian has title Practice Lead)
     delete_button_2 = page.get_by_role("button", name="Delete").nth(1) # zero based
@@ -15,5 +16,9 @@ def test_delete_single_connection(loaded_test_data, my_base_url:str, page: Page)
     delete_button_2.click()
 
     expect(page).to_have_title("Connection Results")
+
+    # check the connections found is one less than previously
+
+    # check the connection we delete isn't there
 
     assert False, "Wire up this function to some real database work"
