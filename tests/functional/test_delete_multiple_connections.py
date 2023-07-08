130f9531b9069bcd0b6a73bd57f890a14c50a635
d4daveshep <david@daveshep.net.nz>
2023-07-08 19:38:57 +1200

    removed delete buttons on each row of connections table and replaced with a checkbox and a button to delete selected test is failing because the form post doesn't do anthing yet

--- tests/functional/test_delete_multiple_connections.py
+++ tests/functional/test_delete_multiple_connections.py
@@ -9,21 +9,22 @@ def test_delete_multiple_connection(loaded_test_data, my_base_url: str, page: Pa
     orig_found_heading = page.get_by_role("heading", name=re.compile("Found [0-9]+ Connections"))
     expect(orig_found_heading).to_contain_text("17")
 
-    # check the in left hand column box for the 2nd row (Brian has title Practice Lead) and 4th row (
-    # TODO continue here
+    # check 2 boxes in the left hand column box
     page.locator("#connection_id_3").check()
     page.locator("#connection_id_12").check()
 
 
     # refresh the page and the connection should not be there
-    delete_button_2.click()
+    page.get_by_role("button", name="Delete Selected").click()
+
+    # TODO continue here - get the page to do something
 
     expect(page).to_have_title("Connection Results")
 
     # check the connections found is one less than previously
     new_found_heading = page.get_by_role("heading", name=re.compile("Found [0-9]+ Connections"))
-    expect(new_found_heading).to_contain_text("16")
+    expect(new_found_heading).to_contain_text("15")
 
     # check the connection we delete isn't there
-    no_row = page.get_by_role("row", name="Brian has title Practice Lead Delete")
-    expect(no_row).to_have_count(0)
+    # no_row = page.get_by_role("row", name="Brian has title Practice Lead Delete")
+    # expect(no_row).to_have_count(0)
